bbc153500fbff3d423989e8a2a0130abec8a02ed
Matt Thomson <matt.thomson@cantab.net>
2013-07-14 17:37:16 +0100

    Complete fetching of OEmbed URLs

--- rembed.py
+++ rembed.py
@@ -1,7 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
+class REmbedError(Exception):
+    pass
+
 class REmbedConsumer:
-	def get_oembed_url(self, url):
-		soup = BeautifulSoup(requests.get(url))
-		return soup.link['href']
+    def get_oembed_url(self, url, format='json'):
+        type = self.get_type(format)
+
+        response = requests.get(url)
+
+        if not response.ok:
+            raise REmbedError('Failed to get %s (status code %s)' % (url, response.status_codes))
+
+        soup = BeautifulSoup(requests.get(url).text)
+        link = soup.find('link', type = type, href = True)
+
+        return link['href'] if link else None
+
+    def get_type(self, format):        
+        if format == 'json':
+            return 'application/json+oembed'
+        elif format == 'xml':
+            return 'text/xml+oembed'
+
+        raise REmbedError('Invalid format %s specified (must be json or xml)' % format)
--- test/consumer_test.py
+++ test/consumer_test.py
@@ -1,15 +1,46 @@
 from rembed import *
 
 from hamcrest import *
-from mock import patch
+from mock import *
 import pytest
 import requests
 
 def test_should_find_oembed_url_using_json_by_default():
+    assert_that(get_oembed_url(), equal_to('http://example.com/oembed?format=json'))
+
+def test_should_find_oembed_url_using_json_when_specified():
+    assert_that(get_oembed_url(format = 'json'), equal_to('http://example.com/oembed?format=json'))
+
+def test_should_find_oembed_url_using_xml_when_specified():
+    assert_that(get_oembed_url(format = 'xml'), equal_to('http://example.com/oembed?format=xml'))
+
+def test_should_return_none_if_link_not_present():
+    assert_that(get_oembed_url(fixture = 'no_json_oembed.html'), none())
+
+def test_should_return_none_if_href_not_present():
+    assert_that(get_oembed_url(fixture = 'json_oembed_no_href.html'), none())
+
+def test_should_return_none_for_invalid_html():
+    assert_that(get_oembed_url(fixture = 'invalid.html'), none())
+
+def test_should_throw_error_when_invalid_format_specified():
+    with pytest.raises(REmbedError):
+        get_oembed_url(format = 'txt')
+
+def test_should_throw_error_on_error_response():
+    with pytest.raises(REmbedError):
+        get_oembed_url(ok = False)
+
+def get_oembed_url(fixture = 'valid_oembed.html', format = None, ok = True):
     with patch('requests.get') as mock_get:
-    	mock_get.return_value = open('test/fixtures/valid_oembed.html').read()
+        response = Mock()
+        response.ok = ok
+        response.text = open('test/fixtures/' + fixture).read()
+        mock_get.return_value = response
 
         consumer = REmbedConsumer()
-        oembed_url = consumer.get_oembed_url('http://example.com')
 
-        assert_that(oembed_url, equal_to('http://example.com/oembed?format=json'))
+        if format:
+            return consumer.get_oembed_url('http://example.com', format)
+        else:
+            return consumer.get_oembed_url('http://example.com')
