953ceca28b00315679695e5215be94f5c3542c51
Matt Thomson <matt.thomson@cantab.net>
2013-07-20 16:30:26 +0100

    Integration test passes

--- rembed/test/integration_test.py
+++ rembed/test/integration_test.py
@@ -3,7 +3,6 @@ from rembed import consumer
 from hamcrest import *
 import pytest
 
-@pytest.mark.xfail
 def test_should_get_correct_embedding():
     embedding = consumer.embed('https://twitter.com/BarackObama/status/266031293945503744')
     assert_that(embedding, contains_string('Four more years.'))
