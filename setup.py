6d3fd11f8e1e4a85ca35077ca2133fd483e9520e
Matt Thomson <matt.thomson@cantab.net>
2013-07-30 20:32:22 +0100

    Add some classifiers

--- setup.py
+++ setup.py
@@ -37,5 +37,18 @@ setup(
         'pytest'
     ],
     
-    cmdclass = {'test': PyTest}
+    cmdclass = {'test': PyTest},
+
+    classifiers = (
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Text Processing'
+    )
 )
