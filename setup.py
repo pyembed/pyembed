df73913dac923586829a5c43d11121343e15f130
Matt Thomson <matt.thomson@cantab.net>
2013-08-02 08:32:00 +0100

    Classifiers should be a list.

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     
     cmdclass = {'test': PyTest},
 
-    classifiers = (
+    classifiers = [
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Natural Language :: English',
@@ -50,5 +50,5 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Topic :: Text Processing'
-    )
+    ]
 )
