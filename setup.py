c199da6683bc70eb03098d73152725d4cf7cea7b
Matt Thomson <mattjohnthomson@gmail.com>
2015-09-05 15:01:26 +0100

    Release v1.3.0

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ class PyTest(TestCommand):
 
 setup(
     name='pyembed',
-    version='1.2.3',
+    version='1.3.0',
     author='Matt Thomson',
     author_email='mattjohnthomson@gmail.com',
     url='http://pyembed.github.io',
